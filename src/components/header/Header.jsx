44c143eb39b10d62ab7f130f55f3ab7c7aff4969
USUARIO <juandavid.estevesgirata@gmail.com>
2021-11-17 12:35:55 -0500

    First Commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,48 @@
+import {Link} from 'react-router-dom'
+import {useSelector} from 'react-redux'
+import axios from 'axios'
+import UserLink from './UserLink'
+
+
+function Header() {
+    const auth = useSelector(state => state.auth)
+
+    const {user, isLogged} = auth
+
+    console.log(auth)
+
+
+    const handleLogout = async () => {
+        try {
+            // await axios.get('/user/logout')
+            localStorage.removeItem('firstLogin')
+            localStorage.removeItem('loggedAgoraUser')
+            window.location.href = "/";
+        } catch (err) {
+            window.location.href = "/";
+        }
+    }
+
+    const transForm = {
+        transform: isLogged ? "translateY(-5px)" : 0
+    }
+
+    return (
+        <header>
+            <div className="logo">
+                <h1><Link to="/">Full Auth</Link></h1>
+            </div>
+
+            <ul style={transForm}>
+                {
+                    isLogged
+                    ? <UserLink user = {user} handleLogout = {handleLogout}/>
+                    :<li><Link to="/login"><i className="fas fa-user"></i> Sign in</Link></li>
+                }
+                
+            </ul>
+        </header>
+    )
+}
+
+export default Header
